79d6b05c68f39aa89bef09b08d42229d32f80fea
Pauline Dejos <22-1-00454@vsu.edu.ph>
2025-03-09 02:22:47 +0800

    Remove Firebase deployment workflows and add Vercel deployment workflow for Flutter web app

--- index.js
+++
@@ -1,9 +0,0 @@
-// var admin = require("firebase-admin");
-
-// // Provide the correct relative path to the serviceAccountKey.json file
-// var serviceAccount = require("./config/serviceAccountKey.json");
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://pawsmatch-c5390-default-rtdb.firebaseio.com"
-// });
