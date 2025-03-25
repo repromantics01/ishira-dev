b6d504b961c814ab65bb57b6bce4a7778db94949
Pauline Dejos <22-1-00454@vsu.edu.ph>
2025-03-25 08:00:45 +0800

    Refactor platform-specific app initialization and error handling

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,39 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'firebase_options.dart';
 
-// Replace any direct import of main_web.dart with this conditional import approach
+// Make sure all imports come before any declarations
 import 'main_stub.dart'
     if (dart.library.html) 'main_web.dart'
     if (dart.library.io) 'main_mobile.dart';
+
+// Define a stub function that will be replaced by platform implementations
+Future<void> initializeApp() async {
+  if (kIsWeb) {
+    // Web initialization is handled in main_web.dart
+    // This function will be completely replaced when building for web
+  } else {
+    // Default mobile initialization
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
   // Initialize Firebase and other services based on platform
-  await initializeApp(); // This function will be defined in platform-specific files
+  await initializeApp();
   
-  runApp(MyApp());
+  runApp(const MyApp());
+}
+
+// Provide a default implementation that will be replaced
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(); // This will be replaced by platform-specific implementations
+  }
 }
